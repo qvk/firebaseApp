493100afc817feae3d6bef2b82bdeae5c162d756
Vikas <vkq0011@gmail.com>
2017-06-18 16:31:36 +0530

    add sliding nav drawer

--- app/src/main/java/com/firebaseapp/demo/MainActivity.java
+++ app/src/main/java/com/firebaseapp/demo/MainActivity.java
@@ -1,10 +1,10 @@
 package com.firebaseapp.demo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,10 +21,11 @@ import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int RC_SIGNIN = 100;
 
+    private Toolbar toolbar;
     private TextView textView;
     private Button signOutButton;
 
@@ -34,6 +35,8 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         textView = (TextView)findViewById(R.id.txt_view);
         signOutButton = (Button)findViewById(R.id.sign_out);
         signOutButton.setVisibility(View.INVISIBLE);
