37df18b024b90e0e767f2393cf7db5c3be4d1877
Vikas <vkq0011@gmail.com>
2017-06-19 12:48:08 +0530

    set up nav drawer and sign out redirect

--- app/src/main/java/com/firebaseapp/demo/MainActivity.java
+++ app/src/main/java/com/firebaseapp/demo/MainActivity.java
@@ -1,14 +1,26 @@
 package com.firebaseapp.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     private Toolbar toolbar;
 
     private FirebaseAuth mAuth;
@@ -17,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,12 +40,59 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        checkUserLogin();
+        if (checkUserLogin()) {
+            // proceed
+            setupNavDrawer();
+        }
     }
 
-    private void checkUserLogin() {
+    private boolean checkUserLogin() {
         // if no user, go to LoginCheckActivity
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            Toast.makeText(this, "user null", Toast.LENGTH_SHORT).show();
+            goToLoginActivity();
+            return false;
+        }
+        return true;
     }
 
+    private void setupNavDrawer() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.home:
+                        Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.movies:
+                        Toast.makeText(getApplicationContext(),"Movies",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.logout:
+                        signOut();
+
+                }
+                return true;
+            }
+        });
+    }
+
+    private void signOut() {
+        AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MainActivity.this, "signed out", Toast.LENGTH_SHORT).show();
+                        goToLoginActivity();
+                    }
+                });
+    }
+
+    private void goToLoginActivity() {
+        startActivity(new Intent(this, LoginCheckActivity.class));
+        finish();
+    }
 
 }
