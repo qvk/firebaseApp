e1c384f853a5885cad41d2872bd106a98b213bfd
Vikas <vkq0011@gmail.com>
2017-06-19 13:41:03 +0530

    set up action bar drawer toggle

--- app/src/main/java/com/firebaseapp/demo/MainActivity.java
+++ app/src/main/java/com/firebaseapp/demo/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -77,6 +79,22 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        // set up action bar drawer toggle
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View v) {
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 
     private void signOut() {
